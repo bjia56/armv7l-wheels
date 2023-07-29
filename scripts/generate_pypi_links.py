cc136a50bf4e117dae2a95348fe893f65c809b41
Brett Jia <dev.bjia56@gmail.com>
2023-07-29 13:10:03 -0400

    update pypi release process

--- scripts/generate_pypi_links.py
+++ scripts/generate_pypi_links.py
@@ -13,9 +13,9 @@ from normalize import normalize
 
 
 INDEX_FILE = "index.html"
+TEMPLATE_FILE = "pkg_template.html"
 CWD = pathlib.Path(__file__).parent.resolve()
 PYPI_DIR = os.path.join(pathlib.Path(CWD).parent.resolve(), "pypi")
-
 
 
 def package_exists(soup, package_name):
@@ -57,6 +57,45 @@ def update(pkg_name: str, link: str) -> None:
         index.write(soup.prettify("utf-8"))
 
 
+def register_or_update(pkg_name: str, link: str) -> None:
+    # Read our index first
+    root_index = os.path.join(PYPI_DIR, INDEX_FILE)
+    with open(root_index) as html_file:
+        soup = BeautifulSoup(html_file, "html.parser")
+    norm_pkg_name = normalize(pkg_name)
+
+    if package_exists(soup, norm_pkg_name):
+        update(pkg_name, link)
+        return
+
+    # Create a new anchor element for our new package
+    last_anchor = soup.find_all('a')[-1]  # Copy the last anchor element
+    new_anchor = copy.copy(last_anchor)
+    new_anchor['href'] = "{}/".format(norm_pkg_name)
+    new_anchor.contents[0].replace_with(pkg_name)
+
+    # Add it to our index and save it
+    last_anchor.insert_after(new_anchor)
+    with open(root_index, 'wb') as index:
+        index.write(soup.prettify("utf-8"))
+
+    # Then get the template, replace the content and write to the right place
+    with open(os.path.join(PYPI_DIR, TEMPLATE_FILE)) as temp_file:
+        template = temp_file.read()
+
+    url = urlparse(link)
+    filename = url.path.split('/')[-1]
+
+    template = template.replace("_package_name", pkg_name)
+    template = template.replace("_link", link)
+    template = template.replace("_filename", filename)
+
+    os.mkdir(os.path.join(PYPI_DIR, norm_pkg_name))
+    package_index = os.path.join(PYPI_DIR, norm_pkg_name, INDEX_FILE)
+    with open(package_index, "w") as f:
+        f.write(template)
+
+
 def main() -> None:
     if len(sys.argv) != 3:
         print(f"Usage: {sys.argv[0]} <tag> <package>")
@@ -68,7 +107,7 @@ def main() -> None:
     release = requests.get(api).json()
     for asset in release["assets"]:
         url = asset["browser_download_url"]
-        update(package, url)
+        register_or_update(package, url)
 
 
 if __name__ == "__main__":
