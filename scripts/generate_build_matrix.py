0c3d8023f6572bf004324dd22250f895f591ac37
Brett Jia <dev.bjia56@gmail.com>
2023-07-30 00:06:15 -0400

    generate build matrix

--- scripts/generate_build_matrix.py
+++ scripts/generate_build_matrix.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 
+import itertools
+import json
 import os
 import pathlib
-import re
+import requests
 from typing import List, TypedDict
 import yaml
-
-from normalize import normalize
 
 
 CWD = pathlib.Path(__file__).parent.resolve()
 PACKAGES_DIR = os.path.join(pathlib.Path(CWD).parent.resolve(), "packages")
+BUILDSPEC_FILE = "build.yaml"
 
 
 class PythonSpec(TypedDict):
@@ -36,9 +37,63 @@ def get_packages() -> List[str]:
     ]
 
 
+def load_buildspec(package: str) -> BuildSpec:
+    buildspec_file = os.path.join(PACKAGES_DIR, package, BUILDSPEC_FILE)
+    if not os.path.isfile(buildspec_file):
+        raise Exception(f"BuildSpec file {buildspec_file} not found")
+
+    with open(buildspec_file, "r") as file:
+        data: BuildSpec = yaml.safe_load(file)
+        return data
+
+
+def get_python_versions(package: str, buildspec: BuildSpec) -> List[str]:
+    return buildspec["python"]["versions"]
+
+
+def get_package_versions(package: str, buildspec: BuildSpec) -> List[str]:
+    if isinstance(buildspec["package"]["versions"], list):
+        return buildspec["package"]["versions"]
+
+    if buildspec["package"]["versions"] != "latest":
+        return [buildspec["package"]["versions"]]
+
+    latest_version = requests.get(f"https://pypi.org/pypi/{package}/json").json()["info"]["version"]
+    return [latest_version]
+
+
+def already_exists(package: str, version: str, python_version: str) -> bool:
+    tag = f"{package}-{version}-cpython{python_version}"
+    api = f"https://api.github.com/repos/bjia56/armv7l-wheels/releases/tags/{tag}"
+    req = requests.get(api)
+    return req.status_code == 200
+
+
 def main() -> None:
     packages = get_packages()
-    pass
+
+    matrix_list = []
+    for package in packages:
+        buildspec = load_buildspec(package)
+
+        python_versions = get_python_versions(package, buildspec)
+        package_versions = get_package_versions(package, buildspec)
+
+        build_list = itertools.product(package_versions, python_versions)
+
+        for build in build_list:
+            version, python_version = build[0], build[1]
+            if not already_exists(package, version, python_version):
+                matrix_list.append(
+                    {
+                        "package": package,
+                        "version": version,
+                        "python_version": python_version,
+                        "strategy": buildspec["strategy"],
+                    }
+                )
+
+    print(json.dumps(matrix_list))
 
 
 if __name__ == "__main__":
