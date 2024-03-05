df965128e5f087d0946c55ab6417c1a3527b2dc6
Brett Jia <dev.bjia56@gmail.com>
2024-03-04 21:48:07 -0500

    Revert "update curl-cffi build process"
    This reverts commit c7e5c5ed561354053a29e3df8e58acaa322ff430.

--- packages/curl_cffi/armv7l_build_wheels.sh
+++ packages/curl_cffi/armv7l_build_wheels.sh
@@ -1,36 +1,29 @@
 #!/bin/bash
 
 PYTHON3_VERSION=$1
-PACKAGE_VERSION=$2
-PACKAGE_NAME=$3
 
 set -e
 
 build_wheel() (
     PY_VER=$1
-    VER=$2
-    PKG=$3
     mkdir build$PY_VER
     cd build$PY_VER
-    pip$PY_VER wheel --no-deps $PKG==$VER --no-binary $PKG --extra-index-url https://bjia56.github.io/armv7l-wheels/ --prefer-binary
+    pip$PY_VER wheel ..
 )
 
 test_wheel() (
     PY_VER=$1
-    PKG=$2
     cd build$PY_VER
-    pip$PY_VER install wheelhouse/$PKG*manylinux*armv7l.whl --extra-index-url https://bjia56.github.io/armv7l-wheels/ --prefer-binary
-    python$PY_VER -c "import $PKG; print($PKG)"
+    pip$PY_VER install wheelhouse/curl_cffi*manylinux*armv7l.whl
+    python$PY_VER -c "import curl_cffi; print(curl_cffi)"
 )
 
 repair_wheel() (
     PY_VER=$1
-    PKG=$2
     cd build$PY_VER
-    auditwheel repair $PKG*armv7l.whl
+    auditwheel repair curl_cffi*armv7l.whl
 )
 
-build_wheel $PYTHON3_VERSION $PACKAGE_VERSION $PACKAGE_NAME
-repair_wheel $PYTHON3_VERSION $PACKAGE_NAME
-# libcurl-impersonate breaks on old glibc due to zig compiler weirdness
-#test_wheel $PYTHON3_VERSION $PACKAGE_NAME
+build_wheel $PYTHON3_VERSION
+repair_wheel $PYTHON3_VERSION
+test_wheel $PYTHON3_VERSION
